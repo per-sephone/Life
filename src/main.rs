da8cb364ca78bc065e3bac0710312f62f913a3fd
Nora Luna <nob@pdx.edu>
2024-01-31 11:09:48 -0800

    hardware random number generator seems to be working?

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,30 @@ use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 use cortex_m_rt::entry;
+use nanorand::{Rng, SeedableRng, pcg64::Pcg64,};
 
 use microbit::{
     board::Board, 
     display::blocking::Display, 
-    hal::{prelude::*, timer}
+    hal::{
+        prelude::*, 
+        timer,
+        Rng as HWRng,
+    }
 };
 
-use nanorand::{pcg64::Pcg64, Rng, SeedableRng};
 
-fn generate_random_board(led_display: &mut [[u8; 5]; 5]) {
-    let mut rng = nanorand::Pcg64::new_seed(1);
+
+fn generate_random_seed(rng: &mut HWRng) -> u128{
+    rng.random_u64() as u128
+}
+
+fn generate_random_board(led_display: &mut [[u8; 5]; 5], rng: &mut HWRng) {
+    let mut rand = nanorand::Pcg64::new_seed(generate_random_seed(rng));
+    
     for row in 0..5 {
         for col in 0..5 {
-            let b: bool = rng.generate();
+            let b: bool = rand.generate();
             led_display[row][col] = b as u8;
         }
     }
@@ -44,21 +54,12 @@ fn complement(led_display: &mut [[u8; 5]; 5]) {
     }
 }
 
-fn all_lights_are_off(led_display: &mut [[u8; 5]; 5]) -> bool {
-    for row in 0..5 {
-        for col in 0..5 {
-            if led_display[row][col] == 1 {
-                return false;
-            }
-        }
-    }
-    true
-}
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = Board::take().unwrap();
+    let mut rng = HWRng::new(board.RNG);
     let mut display: Display = Display::new(board.display_pins);
     let one_hundred_ms = 100; //ms
     let mut delay = timer::Timer::new(board.TIMER0);
@@ -66,11 +67,11 @@ fn main() -> ! {
     let buttons = board.buttons;
     let five_hundred_ms: u32 = 500;
 
-    generate_random_board(&mut led_display);
+    generate_random_board(&mut led_display, &mut rng);
 
     loop {
         while buttons.button_a.is_low().unwrap() {
-            generate_random_board(&mut led_display);
+            generate_random_board(&mut led_display, &mut rng);
             display.show(& mut delay, led_display.clone(), one_hundred_ms);
         }
 
@@ -80,9 +81,9 @@ fn main() -> ! {
             delay.delay_ms(five_hundred_ms);
         }
 
-        if all_lights_are_off(&mut led_display) == true {
+        if done(&mut led_display) == true {
             delay.delay(five_hundred_ms);
-            generate_random_board(&mut led_display);
+            generate_random_board(&mut led_display, &mut rng);
         }
 
         life(&mut led_display);
